45beb91dc7992a2914b27bdfd0446a3937c209fb
Alessandro Viana <116918756+alessandro830@users.noreply.github.com>
2024-02-06 13:10:52 -0300

    Tabuada

--- aula014ex/ex017/script.js
+++ aula014ex/ex017/script.js
@@ -1,1 +1,26 @@
-alert("Olá!")
+function calcular() {
+    var num = window.document.querySelector("input#num").value
+    var tab = window.document.querySelector("select#seltab")
+    if (num.length == 0) {
+        window.alert("[ERRO] Digite algum número!")
+    } else {
+        var n = Number(num)
+        var c = 0
+        tab.innerHTML = ""
+        while (c <= 10) {
+            var item = window.document.createElement("option")
+            item.text = `${n} x ${c} = ${n*c}`
+            item.value = `tab${c}`
+            tab.appendChild(item)
+            c++
+        }
+
+        /*
+        for (c = 0; c <= 10; c++) {
+            var item = window.document.createElement("option")
+            item.text = `${n} x ${c} = ${n*c}`
+            tab.appendChild(item)
+        }
+        */
+    }
+}
