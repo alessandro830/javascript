32e09768dacb5a7537c94b7764996491094b0bd5
Alessandro Viana <116918756+alessandro830@users.noreply.github.com>
2024-03-02 15:32:05 -0300

    Analisador de números

--- aula015/ambiente.js
+++ aula015/ambiente.js
@@ -1,10 +1,11 @@
 let num = [5, 8, 2, 9, 3]
-num.push(1)
-num.sort()
+num.push(1) //Adicionar um ou mais elementos no final
+num.sort() // Ordenar os elementos
 console.log(num)
 console.log(`O vetor tem ${num.length} posições`)
 console.log(`O primeiro valor do vetor é ${num[0]}`)
 let pos = num.indexOf(2)
+console.log(pos)
 if (pos == -1) {
     console.log("O valor não foi encontrado!")
 } else {
